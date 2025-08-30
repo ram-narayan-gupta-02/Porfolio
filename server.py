744c5e484c1550916b17820416916b9545863ebb
Ram Narayan Gupta <147325067+ram-narayan-gupta-02@users.noreply.github.com>
2025-08-30 17:50:41 +0530

    Update server.py

--- server.py
+++ server.py
@@ -87,4 +87,6 @@ def send_email():
     return jsonify({"success": True, "message": "Emails sent."}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
